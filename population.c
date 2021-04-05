ae35428b3c07df1c1212063d32c32746556bb7d7
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-05 02:34:51 +0000

    automated commit by check50 [check50=True]

--- population.c
+++ population.c
@@ -3,27 +3,31 @@
 
 int main(void)
 {
-    int n,m,y,b,d;
+    int n, m, y, b, d;
     
+    // Prompt for start size
     do
     {
         n = get_int("Start Size: ");
     }
-    while(n < 9);
+    while (n < 9);
     
+    // Prompt for end size
     do
     {
         m = get_int("End Size: ");
     }
-    while(m < n);
+    while (m < n);
     
-    for(y = 0; n < m; y++)
+    // Calculate number of years untill we reach threshold
+    for (y = 0; n < m; y++)
     {
-        b = n/3;
-        d = n/4;
+        b = n / 3;
+        d = n / 4;
         n = n + b - d;
     }
     
+    // Print number of years
     {
         printf("Years: %i\n", y);
     }
