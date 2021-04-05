7a98e1341549a429172ce2d72d14594c231c7c75
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-05 02:03:56 +0000

    automated commit by check50 [check50=True]

--- population.c
+++ population.c
@@ -3,25 +3,25 @@
 
 int main(void)
 {
-    int i;
+    float i;
     do
     {
         // Prompt user for the initial population
-        i = get_int("Initial Population: \n");
+        i = get_float("Initial Population: \n");
     }
     while (i < 9);
 
-    int j;
+    float j;
     do
     {
         // Prompt user for the final population
-        j = get_int("Final Population: \n");
+        j = get_float("Final Population: \n");
     }
     while (j < i);
     
     // Calculates the number of years that the population will take to reach the final size
-    int z = 12 * (j - i) / i;
+    float z = 12 * (j - i) / i;
     
     // Inform the user the number of years
-    printf("Years: %i\n", z);
+    printf("Years: %.0f\n", z);
 }
