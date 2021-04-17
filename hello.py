3bcd2c6a73ce21a56940d1844ac2475982fce4b7
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-17 04:45:54 +0000

    automated commit by submit50

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 from cs50 import get_string
 
-answer = get_string("What's your name? ")
-print(f"hello, {answer}")
+answer = get_string("What's your name? ") #asks the user for his name
+print(f"hello, {answer}") #print in the screen hello and the name typed
