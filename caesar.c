5016deb3dd4183a8592952080dffd2b5df0c1bc3
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-29 22:09:19 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -5,12 +5,16 @@
 #include <string.h>
 
 const int TOTAL = 26;
+
 int NUM_TABELA[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
 char LET_TABELAU[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 char LET_TABELAL[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
+string cypher_text(string plainText, int cypher, int key);
+
 int main(int argc, string argv[])
 {
+    
     int key, cypher;
     string plainText;
 
@@ -31,9 +35,9 @@ int main(int argc, string argv[])
     }
 
     plainText = get_string("Plain text: ");
-
+    
     key = atoi(argv[1]);
-
+    
     for (int i = 0; i < strlen(plainText); i++)
     {
         if (isalpha(islower(plainText[i])))
@@ -44,8 +48,9 @@ int main(int argc, string argv[])
                 {
                     cypher = ( NUM_TABELA[i] + key ) % 26;
                 }
-            }
-            printf("%c", LET_TABELAL[cypher]);
+             }
+             plainText[i] = LET_TABELAL[cypher];        
+            
         }
         
         else if (isalpha(isupper(plainText[i])))
@@ -53,21 +58,17 @@ int main(int argc, string argv[])
             for (int j = 0; j < TOTAL; j++)
             {
                 if (plainText[i] == LET_TABELAU[j])
-                {
+                {;
                     cypher = ( NUM_TABELA[i] + key ) % 26;
                 }
             }
-            printf("%c", LET_TABELAU[cypher]);
-        }
-        
-        else
-        {
-            printf("%c", plainText[i]);
+            plainText[i] = LET_TABELAU[cypher];
         }
         
     }
+    printf("%s", plainText);
+    
 }
-
 
 
 
