85c86be6e6118af5277f654b66e00e3b92fec916
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-29 02:41:05 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -1,93 +1,90 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 #include <math.h>
-#include <ctype.h>
 
-// Declaration of all the functions used 
+int num_letters(string texto);
+int num_words(string texto);
+int num_sentences(string texto);
+int grade_level(int total_letters, int total_sentences);
 
-int getNumLetters(string text);
-int getNumSentences(string text);
-int getNumWords(string text);
-int getGradeLevel(int numLetters, int numSentences, int numWords);
-void printGradeLevel(int gradeLevel);
-
-int main(int argc, string argv[])
+int main(void)
 {
-	string text = get_string("Text: "); // Asks for a text from user
-	
-	// Shows the type of every output of the functions
-
-	int numLetters = getNumLetters(text);
-	int numSentences = getNumSentences(text);
-	int numWords = getNumWords(text);
-
-	int gradeLevel = getGradeLevel(numLetters, numSentences, numWords);
-
-	printGradeLevel(gradeLevel);
-
+    int total_letters, total_words, total_sentences, grade;
+    string text;
+    text = get_string("Text: ");
+    
+    total_letters = num_letters(text);
+    total_words = num_words(text);
+    total_sentences = num_sentences(text);
+    grade = grade_level(total_letters, total_sentences);
+    
+    if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (grade <= 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+    
 }
 
-void printGradeLevel(int gradeLevel) // Print the grade level of the text
+int num_letters(string texto)
 {
-	if (gradeLevel <= 1)
-	{
-		printf("Before Grade 1\n");
-	}
-	else if (gradeLevel >= 16)
-	{
-		printf("Grade 16+\n");
-	}
-	else
-	{
-		printf("Grade %i\n", gradeLevel );
-	}
+    int sum = 0;
+    
+    for (int i = 0; i < strlen(texto); i++)
+    {
+        if (texto[i] != ' ' && texto[i] != '.' && texto[i] != '!' && texto[i] != '?' && texto[i] != '\0')
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
 }
 
-int getGradeLevel(int numLetters, int numSentences, int numWords) // Calculate the formula
+int num_words(string texto)
 {
-	float S = (numSentences / (float)numWords) * 100;
-	float L = (numLetters / (float)numWords) * 100;
-
-	return round(0.0588 * L - 0.296 * S - 15.8);
+    int sum = 0;
+    
+    for (int i = 0; i <= strlen(texto); i++)
+    {
+        if (texto[i] == ' ' || texto[i] == '\0')
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
 }
 
-int getNumLetters(string text) // Calculate the number of letters in the text
+int num_sentences(string texto)
 {
-	int numLetters = 0;
-	for (int i = 0; i < strlen(text); i++)
-	{
-		if (isalpha(text[i]))
-		{
-			numLetters += 1;
-
-		}
-	}
-	return numLetters;
+    int sum = 0;
+    
+    for (int i = 0; i <= strlen(texto); i++)
+    {
+        if (texto[i] == '!' || texto[i] == '?' || texto[i] == '.')
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
 }
 
-int getNumWords(string text) // Calculate the number of numbers in the text
+int grade_level(int total_letters, int total_sentences)
 {
-	int numWords = 0;
-	for (int i = 0; i < strlen(text); i++)
-	{
-		if (text[i] == ' ')
-		{
-			numWords += 1;
-		}
-	}
-	return numWords + 1;
-}
-
-int getNumSentences(string text) // Calculate the number of sentences in the text
-{
-	int numSentences = 0;
-	for (int i = 0; i < strlen(text); i++)
-	{
-		if (text[i] == '.' || text[i] == '!' || text[i] == '?')
-		{
-			numSentences += 1;
-		}
-	}
-	return numSentences;
+    int L, S;
+    
+    L = round(total_letters / 100);
+    S = round(total_sentences / 100);
+    
+    int grade = round(0.0588 * L - 0.296 * S - 15.8);
+    return grade;
+    
 }
