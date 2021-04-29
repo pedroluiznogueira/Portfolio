ee30637418f8276ec888eaaece5046da1f6a6a91
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-29 22:43:43 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -10,12 +10,10 @@ int NUM_TABELA[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
 char LET_TABELAU[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 char LET_TABELAL[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-string cypher_text(string plainText, int cypher, int key);
-
 int main(int argc, string argv[])
 {
     
-    int key, cypher;
+    int key, cipher;
     string plainText;
 
     if (argc != 2 || atoi(argv[1]) <= 0)
@@ -40,30 +38,23 @@ int main(int argc, string argv[])
     
     for (int i = 0; i < strlen(plainText); i++)
     {
-        if (isalpha(islower(plainText[i])))
+        if (isalpha(plainText[i]))
         {
-            for (int j = 0; j < TOTAL; j++)
+            for (int j = 0; j < strlen(plainText); j++)
             {
-                if (plainText[i] == LET_TABELAL[j])
+                if (islower(plainText[j]) == LET_TABELAL[j])
                 {
-                    cypher = ( NUM_TABELA[i] + key ) % 26;
+                    cipher = (plainText[j] + key) % 26;
+                    plainText[j] = LET_TABELAL[j];
+                    
                 }
-                plainText[i] = LET_TABELAL[cypher];
+                else if (isupper(plainText[j]) == LET_TABELAU[j])
+                {
+                    cipher = (plainText[j] + key) % 26;
+                    plainText[j] = LET_TABELAU[j];
+                }
             }
         }
-        
-        else if (isalpha(isupper(plainText[i])))
-        {
-            for (int j = 0; j < TOTAL; j++)
-            {
-                if (plainText[i] == LET_TABELAL[j])
-                {
-                    cypher = ( NUM_TABELA[i] + key ) % 26;
-                }
-                plainText[i] = LET_TABELAL[cypher];
-            }
-        }
-    
     }
     
     printf("ciphertext: %s", plainText);
