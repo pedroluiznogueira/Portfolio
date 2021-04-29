f371f43ba8d8d26131b97ca0d5e592d8fdf7f018
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-29 13:27:30 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -8,31 +8,20 @@ int getnum_letters(string text);
 int getnum_words(string text);
 int getnum_sentences(string text);
 int calculate_grade(int num_letters, int num_sentences, int num_words);
+void printgrade_level(int grade_level);
 
 int main(void)
 {
-    int num_letters, num_sentences, num_words;
+    int num_letters, num_sentences, num_words, grade_level;
     string texto;
 
     texto = get_string("Text: ");
     num_letters = getnum_letters(texto);
     num_sentences = getnum_sentences(texto);
     num_words = getnum_words(texto);
-    float grade_level = calculate_grade(num_letters, num_sentences, num_words);
-
-    if (grade_level >= 16)
-    {
-        printf("Grade 16+\n");
-    }
-    else if (grade_level <= 1)
-    {
-        printf("Before Grade 1\n");
-    }
-    else
-    {
-        printf("Grade %i\n", (int) grade_level);
-    }
-
+    grade_level = calculate_grade(num_letters, num_sentences, num_words);
+    
+    printgrade_level(grade_level);
 }
 
 int getnum_letters(string text)
@@ -85,6 +74,21 @@ int calculate_grade(int num_letters, int num_sentences, int num_words)
     return floor(0.0588 * L - 0.296 * S - 15.8);
 }
 
+void printgrade_level(int grade_level)
+{
+    if (grade_level >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (grade_level <= 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade_level);
+    }
+}
 
 
 
