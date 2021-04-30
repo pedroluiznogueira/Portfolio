75c83bee8ab4b06cbb403a0cbb370a743a47bdb9
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-30 00:10:58 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 const int TOTAL = 26;
 
 int NUM_TABELA[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
-char LET_TABELAU[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-char LET_TABELAL[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char POINTS[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char ALPHA[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 int main(int argc, string argv[])
 {
+    int cipher;
     
-    int key, cipher;
-    string plainText;
-
     if (argc != 2 || atoi(argv[1]) <= 0)
     {
         printf("Erro\n");
@@ -32,33 +30,37 @@ int main(int argc, string argv[])
 
     }
 
-    plainText = get_string("Plain text: ");
-    key = atoi(argv[1]);
+    string word = get_string("Plain text: ");
+    int key = atoi(argv[1]);
     
-    
-    for (int i = 0; i < strlen(plainText); i++)
+
+    for (int i = 0; i < strlen(word); i++)
     {
-        if (isalpha(plainText[i]))
+        if (islower(word[i]))
         {
-            for (int j = 0; j < strlen(plainText); j++)
+            for (int j = 0; j < TOTAL; j++)
             {
-                if (islower(plainText[j]) == LET_TABELAL[j])
+                if (word[i] == ALPHA[j])
                 {
                     cipher = (NUM_TABELA[j] + key) % 26;
-                    plainText[j] = LET_TABELAL[cipher];
-                    
-                }
-                else if (isupper(plainText[j]) == LET_TABELAU[j])
-                {
-                    cipher = (NUM_TABELA[j] + key) % 26;
-                    plainText[j] = LET_TABELAU[cipher];
+                    word[i] = ALPHA[cipher];
                 }
             }
         }
+        else if (isupper(word[i]))
+        {
+            for (int j = 0; j < TOTAL; j++)
+            {
+                if (word[i] == ALPHA[j])
+                {
+                    cipher = (NUM_TABELA[j] + key) % 26;
+                    word[i] = POINTS[cipher];
+                }
+            }
+        }    
+        
     }
-    
-    printf("ciphertext: %s", plainText);
-    
+    printf("%s\n", word);
 }
 
 
