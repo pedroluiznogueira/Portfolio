f4c61c625099d50fcebc1d4173f0a0961294a2b1
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-30 01:42:20 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+const int TOTAL = 26;
+
 int numeros[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
 char letrasmaiu[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 char letrasmin[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
@@ -36,23 +38,25 @@ int main(int argc, string argv[])
     {
         if (islower(word[i]))
         {
-            for (int j = 0; j < strlen(word); j++)
+            for (int j = 0; j < TOTAL; j++)
             {
                 if (word[i] == letrasmin[j])
                 {
                     cipher = (numeros[j] + key) % 26;
                     word[i] = letrasmin[cipher];
+                    j = j + 26;
                 }
             }
         }
         else if (isupper(word[i]))
         {
-            for (int j = 0; j < strlen(word); j++)
+            for (int j = 0; j < TOTAL; j++)
             {
                 if (word[i] == letrasmaiu[j])
                 {
                     cipher = (numeros[j] + key) % 26;
                     word[i] = letrasmaiu[cipher];
+                    j = j + 26;
                 }
             }
         }
