92281d85781e86bede53e12456789defa5b54e2f
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-04-30 00:58:39 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -4,11 +4,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-const int TOTAL = 26;
-
-int NUM_TABELA[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
-char POINTS[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-char ALPHA[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+int numeros[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+char letrasmaiu[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char letrasmin[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 int main(int argc, string argv[])
 {
@@ -38,23 +36,23 @@ int main(int argc, string argv[])
     {
         if (islower(word[i]))
         {
-            for (int j = 0; j < TOTAL; j++)
+            for (int j = 0; j < strlen(word); j++)
             {
-                if (word[j] == ALPHA[j])
+                if (word[i] == letrasmin[j])
                 {
-                    cipher = (NUM_TABELA[j] + key) % 26;
-                    word[i] = ALPHA[cipher];
+                    cipher = (numeros[j] + key) % 26;
+                    word[i] = letrasmin[cipher];
                 }
             }
         }
         else if (isupper(word[i]))
         {
-            for (int j = 0; j < TOTAL; j++)
+            for (int j = 0; j < strlen(word); j++)
             {
-                if (word[j] == ALPHA[j])
+                if (word[i] == letrasmaiu[j])
                 {
-                    cipher = (NUM_TABELA[j] + key) % 26;
-                    word[i] = POINTS[cipher];
+                    cipher = (numeros[j] + key) % 26;
+                    word[i] = letrasmaiu[cipher];
                 }
             }
         }
