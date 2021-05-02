0d4e40c80e00944cba76799bb61616cb2c16acf4
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-05-02 00:15:49 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -80,8 +80,6 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int sum = 0;
-
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++ )
