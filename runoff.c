982d91338873e9382a41f0ccade34b133d5b6bf7
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-05-03 00:35:16 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -190,12 +190,9 @@ bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        if (candidates[i].votes != min)
         {
-            if (candidates[i].votes != candidates[j].votes)
-            {
-                return false;
-            }
+            return false;
         }
     }
     return true;
