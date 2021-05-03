583618ea736de376095d98f1ebdec87767532f60
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-05-03 23:03:22 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -37,12 +37,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int originalRed = image[i][j].rgbtRed;
-            int originalBlue = image[i][j].rgbtBlue;
-            int originalGreen = image[i][j].rgbtGreen;
-            image[i][j].rgbtRed = cap(round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue));
-            image[i][j].rgbtGreen = cap(round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue));
-            image[i][j].rgbtBlue = cap(round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue));
+            image[i][j].rgbtRed = cap(round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue));
+            image[i][j].rgbtGreen = cap(round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue));
+            image[i][j].rgbtBlue = cap(round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue));
         }
 
     }
