17a628b0f56d56dbbd856632e2a96525729e7329
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-05-03 20:53:39 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -19,6 +19,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 
                 }
             }
+            else if (image[i][j].rgbtRed == image[i][j].rgbtGreen)
+            {
+                image[i][j].rgbtBlue = image[i][j].rgbtRed;
+                
+            }
+            else if (image[i][j].rgbtRed == image[i][j].rgbtBlue)
+            {
+                image[i][j].rgbtGreen = image[i][j].rgbtRed;
+                
+            }
         }
     }
     return;
