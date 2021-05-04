f2843e0a20ab41a1716d7417e854eca55f6ae073
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-05-04 18:44:35 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -2,73 +2,72 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define BLOCK_SIZE 512
+#define BLOCK_SIZE 512 // Setting the size of Bytes that each chunck of memory has, relationing it to buffer
 
-typedef uint8_t BYTE;
+typedef uint8_t BYTE; // uint8_t is a data type in C wich stores 8 bits/1 byte
 
-void checkArgumentCount(int argc);
-void checkFileExists(FILE *file);
-int isJPEG(BYTE buffer[]);
- 
+void checkUsage(int argc); //  Function that will check if the command line argument are correct
+void verifyNull(FILE *file); // Verifying if the file that was just opened is not NULL
+int verifyJPEG(BYTE buffer[]); // Verifying if the first bytes of the chunck buffer match with the ones that a JPEG file has
+
 int main(int argc, char *argv[])
 {
-    checkArgumentCount(argc);
-    FILE *raw = fopen(argv[1], "r");
-    checkFileExists(raw);
-    
-    // Memory requiered is going to be allocated and initialize variable
+    checkUsage(argc);
+    FILE *memorycard = fopen(argv[1], "r"); // Opening memory card to verify each chunck of memory
+    verifyNull(memorycard);
+
+    // Created a array of buffer, wich each oe will be from the data type BYTE(one byte)
+    // It will has 512 of them
+    // So I will be able to verify each first buffer of a file 512 times, to compare in the proportion I need, with a JPEG
     BYTE buffer[BLOCK_SIZE];
-    
+
     //Buffer for filename
-    
+
+    FILE *newjpg; // Creating a pointer to a file named image
     char filename[8];
-    FILE *image;
     int counter = 0;
-    
-    // After First JPEG iamge is found, all are adjacent
-    while (fread(buffer, BLOCK_SIZE, 1, raw) == 1)
+
+    // If I find a JPEG file, the next one will be right next each other
+    while (fread(buffer, BLOCK_SIZE, 1, memorycard) == 1) // Buffer is where I am storing the data I am reading
     {
-        if (isJPEG(buffer) == 1)
+        if (verifyJPEG(buffer) == 1) // One at a time buffers will be storing 512 bytes from the memory card, so I am checking if they are in fact a JPEG file
         {
             // Close the previous image, unless it's the first JPEG
-            if (counter != 0)
+            if (counter != 0) // -- SEE SECOND -- So we will be closing every file after already doing what we needed
             {
-                fclose(image);
+                fclose(newjpg);
             }
-            sprintf(filename, "%03i.jpg", counter++);
-            image = fopen(filename, "w");
-            fwrite(buffer, BLOCK_SIZE, 1, image);
+            sprintf(filename, "%03i.jpg", counter++); // (wheretowrite, whattobewritten, addingtocounter) -- SEE FIRST --
+            newjpg = fopen(filename, "w"); // opening a file that image points
+            fwrite(buffer, BLOCK_SIZE, 1, newjpg); // for each found in the start of the while loop, will be written the file found on image
+
         }
-        // Next blocks of current JPEG file 
-        //Will be written until we find next JPEG header
-        
+        // Unless we find another JPEG file, we will be writing to image the blocks of the current JPEG file
         else if (counter > 0)
         {
-            fwrite(buffer, BLOCK_SIZE, 1, image);
+            fwrite(buffer, BLOCK_SIZE, 1, newjpg);
         }
     }
- 
+
 }
 
-void checkArgumentCount(int argc)
+void checkUsage(int argc)
 {
     if (argc != 2)
     {
-        printf("Usage: ./recover image\n");
         exit(1);
     }
 }
 
-void checkFileExists(FILE *file)
+void verifyNull(FILE *file)
 {
     if (file == NULL)
     {
-        printf("File could not be opened.\n");
         exit(1);
     }
 }
 
-int isJPEG(BYTE buffer[])
+int verifyJPEG(BYTE buffer[])
 {
     if (buffer[0] == 0Xff && buffer[1] == 0Xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
