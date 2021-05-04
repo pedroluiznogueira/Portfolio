e5f4ebd8909a84f3995a332daf37dd4b8982dfb6
pedroluiznogueira <pedroluiznogueira@users.noreply.github.com>
2021-05-04 00:02:53 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -63,7 +63,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width / 2; j++)
         {
             swap(&image[i][j], &image[i][width - (j + 1)]);
         }
